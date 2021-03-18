e8ff7bd5cd5b40eeba47ca0ef61c0a68b69fb2e0
SyngyeonTak <67184044+SyngyeonTak@users.noreply.github.com>
2021-03-18 20:10:32 +0900

    first commit

---
+++ src/test/java/com/study/day0318/Day0318ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.day0318;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day0318ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
